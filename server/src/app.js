dfff50eeff059233cfb925bedfbce78b8c3c8c57
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2022-03-09 17:38:53 +0530

    added  morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path= require('path')
 const cors = require('cors') ;
+const morgan = require('morgan')
 const planetRouter = require('./routes/router.planets')
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors({
     origin:'http://localhost:3000'
 }));
+app.use(morgan('combined'))
 app.use(express.json()) ;
 app.use(express.static(path.join(__dirname,'..','public','build')))
 app.use('/planets',planetRouter) ;
